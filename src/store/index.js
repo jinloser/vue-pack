7a785792047c580e299a8be27172290ece49462f
jinloser <jinwenqiu@foxmail.com>
2021-05-26 14:01:38 +0800

    pro init

--- src/store/index.js
+++ src/store/index.js
@@ -4,13 +4,13 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  // 全局 state
-  state: {},
-  // state中数据的get方法, 用于数据的初步包装
-  getters: {},
-  // 即时/非异步 修改 state 中数据的方法
-  mutations: {},
-  // 异步修改 state 中数据的方法
-  actions: {},
-  modules: {},
+    // 全局 state
+    state: {},
+    // state中数据的get方法, 用于数据的初步包装
+    getters: {},
+    // 即时/非异步 修改 state 中数据的方法
+    mutations: {},
+    // 异步修改 state 中数据的方法
+    actions: {},
+    modules: {},
 });
