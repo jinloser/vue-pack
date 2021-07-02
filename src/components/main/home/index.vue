b9ed2781a76f74fc76f9dd91679125b37751199c
jinloser <jinwenqiu@foxmail.com>
2021-07-02 09:06:39 +0800

    style 修改全局样式

--- src/components/main/home/index.vue
+++ src/components/main/home/index.vue
@@ -1,10 +1,9 @@
 <template>
-  <div>home</div>
+    <div>home</div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
